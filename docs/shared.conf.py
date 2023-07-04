0440b4176d4a18140bc619da5fd0936fb9606441
ebs111222 <49143924+ebs111222@users.noreply.github.com>
2023-07-04 12:45:33 +0300

    Update shared.conf.py

--- docs/shared.conf.py
+++ docs/shared.conf.py
@@ -88,7 +88,7 @@ release = u'3.1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-#language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -246,7 +246,7 @@ html_sidebars = {
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
+html_search_language = 'en'
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
@@ -316,7 +316,7 @@ epub_copyright = copyright
 
 # The language of the text. It defaults to the language option
 # or 'en' if the language is not set.
-#epub_language = ''
+epub_language = 'en'
 
 # The scheme of the identifier. Typical schemes are ISBN or URL.
 #epub_scheme = ''
